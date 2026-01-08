c32e117eed0eb645456cb1b09d4f7692ec380934
ghshart <ghsh.art@gmail.com>
2026-01-07 21:20:48 -0600

    Add files via upload

---
+++ github.py
@@ -0,0 +1,20 @@
+print("Hello git hub world")
+guess = int(input('Please guess a number between 1 to 10: '))
+secret_number = 3
+while guess != secret_number:
+    if (guess)==(secret_number):
+        print("Yai !! Correct")
+        print()
+    elif guess > 10: 
+        print("try a number smaller then 10. Try again.")
+        print( )
+        guess = int(input('Please guess a number between 1 to 10: '))
+        print( )
+    else:
+        (guess) != secret_number
+        print("Try again !")
+        print( )
+        guess = int(input('Please guess a number between 1 to 10: '))
+        print()
+exit
+
